1a6e8ae1bdfeef3df23aeb5d03dce6e3bf2b598e
RIPBurak <burak.yilmaz@2ag.com.tr>
2023-12-14 09:05:11 +0300

    MovementHeader

--- backend/QuickSell/src/QuickSell.HttpApi/Controllers/MovementHeaders/MovementHeadersController.cs
+++ backend/QuickSell/src/QuickSell.HttpApi/Controllers/MovementHeaders/MovementHeadersController.cs
@@ -1,22 +1,15 @@
-
-
-
-
 using System;
 using System.Threading.Tasks;
-using Volo.Abp;
 using Volo.Abp.AspNetCore.Mvc;
-using Volo.Abp.Application.Dtos;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 using QuickSell.MovementHeaders;
+using System.Collections.Generic;
+using QuickSell.Shared;
+using DevExtreme.AspNet.Data.ResponseModel;
 
-
-
-
-namespace  QuickSell.Controllers.MovementHeaders
+namespace QuickSell.Controllers.MovementHeaders
 {
-    
+
     [Route("api/movement-headers")]
     
     public abstract class MovementHeadersController : AbpController,IMovementHeadersAppService
@@ -26,9 +19,38 @@ namespace  QuickSell.Controllers.MovementHeaders
         
 
         public MovementHeadersController(IMovementHeadersAppService movementHeadersAppService)
-       {
-        _movementHeadersAppService = movementHeadersAppService;
-       }
-
+        {
+             _movementHeadersAppService = movementHeadersAppService;
+        }
+        [HttpPost]
+        [Route("300101")]
+        public async Task<MovementHeaderDto> AddMovementHeader(MovementHeaderDto input)
+        {
+            return await _movementHeadersAppService.AddMovementHeader(input);
+        }
+        [HttpGet]
+        [Route("300104")]
+        public async Task<LoadResult> GetListMovementHeader(DataSourceLoadOptions loadOptions)
+        {
+            return await _movementHeadersAppService.GetListMovementHeader(loadOptions);
+        }
+        [HttpGet]
+        [Route("300105/{id}")]
+        public async Task<DxMovementHeaderLookupDto?> GetMovementHeaderByID(Guid? id)
+        {
+            return await _movementHeadersAppService.GetMovementHeaderByID(id);
+        }
+        [HttpDelete]
+        [Route("300103/{id}")]
+        public async Task DeleteMovementHeader(Guid id)
+        {
+            await _movementHeadersAppService.DeleteMovementHeader(id);
+        }
+        [HttpPut]
+        [Route("300102/{id}")]
+        public async Task<MovementHeaderDto> UpdateMovementHeader(Guid id, IDictionary<string, object> input)
+        {
+            return await _movementHeadersAppService.UpdateMovementHeader(id, input);
+        }
     }
 }
