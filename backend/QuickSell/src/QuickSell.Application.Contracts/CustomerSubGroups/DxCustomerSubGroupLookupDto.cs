14dc2a57c5270e9bca1e20cf058a62aac92d8c58
RIPBurak <burak.yilmaz@2ag.com.tr>
2023-12-21 09:43:01 +0300

    CustomerSubGroup ve StockSubGroup backend metotları eklendi.

---
+++ backend/QuickSell/src/QuickSell.Application.Contracts/CustomerSubGroups/DxCustomerSubGroupLookupDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSell.CustomerSubGroups
+{
+    public class DxCustomerSubGroupLookupDto
+    {
+        public Guid Id { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+    }
+}
--- backend/QuickSell/src/QuickSell.Application/StockSubGroups/StockSubGroupsAppService.cs
+++ backend/QuickSell/src/QuickSell.Application/StockSubGroups/StockSubGroupsAppService.cs
@@ -1,4 +1,14 @@
+using DevExtreme.AspNet.Data.ResponseModel;
+using DevExtreme.AspNet.Data;
+using QuickSell.Tools;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Volo.Abp;
 using Volo.Abp.Application.Services;
+using QuickSell.Shared;
+using System.Linq;
+using Volo.Abp.Data;
 
 namespace QuickSell.StockSubGroups
 {
@@ -6,12 +16,64 @@ namespace QuickSell.StockSubGroups
     {
         private readonly IStockSubGroupRepository _stockSubGroupRepository;
         private readonly StockSubGroupManager _stockSubGroupManager;
+        private readonly IDataFilter _dataFilter;
 
-        public StockSubGroupsAppService(IStockSubGroupRepository stockSubGroupRepository, StockSubGroupManager stockSubGroupManager)
+        public StockSubGroupsAppService(IStockSubGroupRepository stockSubGroupRepository,
+                                        StockSubGroupManager stockSubGroupManager,
+                                        IDataFilter dataFilter)
         {
             _stockSubGroupRepository = stockSubGroupRepository;
             _stockSubGroupManager = stockSubGroupManager;
+            _dataFilter = dataFilter;
         }
+        public async Task<LoadResult> GetListStockSubGroup(DataSourceLoadOptions loadOptions)
+        {
+            var getStockSubGroup = await _stockSubGroupRepository.GetQueryableAsync();
 
+            var getJoinedData = from stksbgrp in getStockSubGroup
+                                select new DxStockSubGroupLookupDto
+                                {
+                                    Id = stksbgrp.Id,
+                                    Code = stksbgrp.Code,
+                                    Name = stksbgrp.Name
+                                };
+            return await DataSourceLoader.LoadAsync(getJoinedData, loadOptions);
+        }
+        public async Task<DxStockSubGroupLookupDto?> GetStockSubGroupByID(Guid? id)
+        {
+            var dataFilter = _dataFilter.Disable<ISoftDelete>();
+            using (dataFilter)
+            {
+                var getStockSubGroup = (await _stockSubGroupRepository.GetQueryableAsync());
+                var stockSubGroups = (from stksbgrp in getStockSubGroup
+                                   where stksbgrp.Id == id
+                                   select new DxStockSubGroupLookupDto
+                                   {
+                                       Id = stksbgrp.Id,
+                                       Code = stksbgrp.Code,
+                                       Name = stksbgrp.Name,
+                                   }).FirstOrDefault();
+                return stockSubGroups;
+            }
+        }
+        public async Task<StockSubGroupDto> AddStockSubGroup(StockSubGroupDto input)
+        {
+            var stockSubGroup = await _stockSubGroupManager.CreateAsync(
+              input.Code,
+              input.Name
+          );
+            return ObjectMapper.Map<StockSubGroup, StockSubGroupDto>(stockSubGroup);
+        }
+        public async Task<StockSubGroupDto> UpdateStockSubGroup(Guid id, IDictionary<string, object> input)
+        {
+            var stockSubGroup = await _stockSubGroupRepository.GetAsync(id);
+            var updated = await DevExtremeUpdate.Update(stockSubGroup, input);
+            await _stockSubGroupRepository.UpdateAsync(updated);
+            return ObjectMapper.Map<StockSubGroup, StockSubGroupDto>(updated);
+        }
+        public async Task DeleteStockSubGroup(Guid id)
+        {
+            await _stockSubGroupRepository.DeleteAsync(id);
+        }
     }
 }
