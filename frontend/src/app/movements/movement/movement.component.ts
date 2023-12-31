a372e2e6da86e5f6ad526b7ebbc9bff20b210186
Serkan Karahan <serkan.karahan@2ag.com.tr>
2023-12-31 19:09:42 +0300

    hareket sayfası eklendi. Hesaplama metotları yazıldı

--- frontend/src/app/movements/movement/movement.component.ts
+++ frontend/src/app/movements/movement/movement.component.ts
@@ -13,6 +13,10 @@ import Swal from 'sweetalert2';
   styleUrls: ['./movement.component.css']
 })
 export class MovementComponent {
+
+  customerCardLookup:any;
+  stockCardLookup:any;
+
   successButtonOptions: any;
   cancelButtonOptions: any;
   copyButtonOptions: any;
@@ -33,7 +37,7 @@ export class MovementComponent {
   constructor(private http: HttpClient, private toastr:ToastrService) {
     this.dataSource = new CustomStore({
       key: 'id',
-      load: () => this.sendRequest('https://localhost:44369/100104'),
+      load: () => this.sendRequest('https://localhost:44369/300104'),
     });
     this.successButtonOptions = {
       type: 'success',
@@ -52,7 +56,75 @@ export class MovementComponent {
         this.dataGrid.instance.cancelEditData();
       },
     };
+
+    this.http.get('https://localhost:44369/200304').subscribe((res:any) => {
+      this.customerCardLookup = res.data;
+    })
+    this.http.get('https://localhost:44369/100104').subscribe((res:any) => {
+      this.stockCardLookup = res.data;
+    })
   }
+  setCellValueQuantity(newData:any, value:any, currentRowData:any) {
+    newData.quantity = value
+    if(currentRowData.price && !currentRowData.discountRate && !currentRowData.vatRate){
+      newData.amount = value * currentRowData.price
+      newData.finalAmount = value * currentRowData.price
+    }
+    else if (currentRowData.price && currentRowData.discountRate && !currentRowData.vatRate){
+      newData.amount = value * currentRowData.price
+      newData.discountAmount = (value * currentRowData.price) * (currentRowData.discountRate / 100)
+      newData.finalAmount = (value * currentRowData.price) - newData.discountAmount
+    }
+    else if (currentRowData.price && currentRowData.discountRate && currentRowData.vatRate){
+      newData.amount = value * currentRowData.price
+      newData.discountAmount = (value * currentRowData.price) * (currentRowData.discountRate / 100)
+      newData.vatAmount = ((value * currentRowData.price) - newData.discountAmount) * (currentRowData.vatRate / 100)
+      newData.finalAmount = ((value * currentRowData.price) - newData.discountAmount) + newData.vatAmount
+    }
+    else if (currentRowData.price && !currentRowData.discountRate && currentRowData.vatRate){
+      newData.amount = value * currentRowData.price
+      newData.vatAmount = (value * currentRowData.price) * (currentRowData.vatRate / 100)
+      newData.finalAmount = (value * currentRowData.price) + newData.vatAmount
+    }
+}
+setCellValuePrice(newData:any, value:any, currentRowData:any) {
+  newData.price = value
+  if(currentRowData.quantity && !currentRowData.discountRate && !currentRowData.vatRate){
+    newData.amount = value * currentRowData.quantity
+    newData.finalAmount = value * currentRowData.quantity
+  }
+  else if (currentRowData.quantity && currentRowData.discountRate && !currentRowData.vatRate){
+    newData.amount = value * currentRowData.quantity
+    newData.discountAmount = (value * currentRowData.quantity) * (currentRowData.discountRate / 100)
+    newData.finalAmount = (value * currentRowData.quantity) - newData.discountAmount
+  }
+  else if (currentRowData.quantity && currentRowData.discountRate && currentRowData.vatRate){
+    newData.amount = value * currentRowData.quantity
+    newData.discountAmount = (value * currentRowData.quantity) * (currentRowData.discountRate / 100)
+    newData.vatAmount = ((value * currentRowData.quantity) - newData.discountAmount) * (currentRowData.vatRate / 100)
+    newData.finalAmount = ((value * currentRowData.quantity) - newData.discountAmount) + newData.vatAmount
+  }
+  else if (currentRowData.quantity && !currentRowData.discountRate && currentRowData.vatRate){
+    newData.amount = value * currentRowData.quantity
+    newData.vatAmount = (value * currentRowData.quantity) * (currentRowData.vatRate / 100)
+    newData.finalAmount = (value * currentRowData.quantity) + newData.vatAmount
+  }
+
+}
+setCellValueDiscountRate(newData:any, value:any, currentRowData:any) {
+ if(currentRowData.amount){
+  newData.discountRate = value
+  newData.discountAmount = (value / 100) * currentRowData.amount
+  newData.finalAmount = currentRowData.amount - newData.discountAmount
+ }
+}
+setCellValueVATRate(newData:any, value:any, currentRowData:any) {
+  if(currentRowData.amount && currentRowData.discountAmount){
+   newData.vatRate = value
+   newData.vatAmount = (value / 100) * (currentRowData.amount - currentRowData.discountAmount)
+   newData.finalAmount = currentRowData.amount - currentRowData.discountAmount + newData.vatAmount
+  }
+ }
   sendRequest(url: string, method = 'GET', data: any = {}): any {
     try {
       this.logRequest(method, url, data);
@@ -99,13 +171,13 @@ export class MovementComponent {
   onRowInserting(e: any) {
     e.cancel = true
     try {
-      this.http.post('https://localhost:44369/100101', e.data).subscribe(
+      this.http.post('https://localhost:44369/300101', e.data).subscribe(
         (res: any) => {
           this.toastr.success('Data saved successfully', 'Success', {
             closeButton: true,
             timeOut: 5000
           });
-          this.http.get('https://localhost:44369/100104').subscribe((res:any) => {
+          this.http.get('https://localhost:44369/300104').subscribe((res:any) => {
             this.dataSource = res.data
           })
           e.component.cancelEditData();
@@ -124,12 +196,12 @@ export class MovementComponent {
   onRowUpdating(e:any){
     e.cancel = true
     try {
-      this.http.put('https://localhost:44369/100102/' + e.key, e.newData).subscribe((res:any) => {
+      this.http.put('https://localhost:44369/300102/' + e.key, e.newData).subscribe((res:any) => {
         this.toastr.success('Data updated successfully', 'Success', {
           closeButton: true,
           timeOut: 5000
         });
-        this.http.get('https://localhost:44369/100104').subscribe((res:any) => {
+        this.http.get('https://localhost:44369/300104').subscribe((res:any) => {
             this.dataSource = res.data
           })
         e.component.cancelEditData();
@@ -147,13 +219,13 @@ export class MovementComponent {
   onRowRemoving(e:any) {
     e.cancel = true
     try {
-      this.http.delete('https://localhost:44369/100103/' + e.key).subscribe((res:any) => {
+      this.http.delete('https://localhost:44369/300103/' + e.key).subscribe((res:any) => {
         debugger
         this.toastr.success('Data removed successfully', 'Success', {
           closeButton: true,
           timeOut:5000
         });
-        this.http.get('https://localhost:44369/100104').subscribe((res:any) => {
+        this.http.get('https://localhost:44369/300104').subscribe((res:any) => {
             this.dataSource = res.data
           })
         e.component.cancelEditData();
